747c9c7069f93474eeea31ada477dff221bdff58
sakshamsomra <saksham.somra@gmail.com>
2022-09-21 15:45:41 +0530

    p

---
+++ planet.js
@@ -0,0 +1,89 @@
+function Earth(){
+
+    let n = document.getElementById("number").value;
+
+    document.getElementById("p1").innerHTML = n;
+
+}
+
+
+
+function Venus(){
+
+    let d = document.getElementById("number").value;
+
+
+    document.getElementById("p3").innerHTML = (d*0.91).toFixed(2);
+
+}
+
+function Mars(){
+
+    let x = document.getElementById("number").value;
+
+    document.getElementById("p2").innerHTML = (x*0.38).toFixed(2);
+
+}
+
+function Jupiter(){
+
+    let d = document.getElementById("number").value;
+
+
+    document.getElementById("p4").innerHTML = (d*2.34).toFixed(2);
+
+}
+
+function Saturn(){
+
+    let d = document.getElementById("number").value;
+
+
+    document.getElementById("p5").innerHTML = (d*1.06).toFixed(2);
+
+}
+
+function Neptune(){
+
+    let d = document.getElementById("number").value;
+
+
+    document.getElementById("p6").innerHTML = (d*1.19).toFixed(2);
+
+}
+
+function Uranus(){
+
+    let d = document.getElementById("number").value;
+
+
+    document.getElementById("p7").innerHTML = (d*0.92).toFixed(2);
+
+}
+
+function Mercury(){
+
+    let d = document.getElementById("number").value;
+
+
+    document.getElementById("p8").innerHTML = (d*0.38).toFixed(2);
+
+}
+
+function Pluto(){
+
+    let d = document.getElementById("number").value;
+
+
+    document.getElementById("p9").innerHTML = (d*0.06).toFixed(2);
+
+}
+
+function Moon(){
+
+    let d = document.getElementById("number").value;
+
+
+    document.getElementById("p10").innerHTML = (d*0.16).toFixed(2);
+
+}
